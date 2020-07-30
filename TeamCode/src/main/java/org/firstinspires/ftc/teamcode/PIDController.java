915e31ad4d51dd91d00552e48335973500323fa3
Clayton-Toste <claytontoste@gmail.com>
2020-07-30 13:05:19 -0700

    Fixed time function and typos in PIDController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -1,30 +1,32 @@
+import java.lang.System;   
+
 public class PIDController {
     private final double projected;
     private final double integral;
     private final double derivative;
-    private int previousTime;
+    private long previousTime;
     private double previousError = 0;
     private double previousIntegral = 0;
 
     public PIDController(double projected, double intergral, double derivative)
     {
         this.projected = projected;
-        this.intergral = intergral;
+        this.integral = intergral;
         this.derivative = derivative;
-        previousTime = getTime();
+        previousTime = System.currentTimeMillis();
     }
 
     public double update(double error)
     {
-        int time = getTime();
-        int deltaTime = time - previousTime;
+        long time = System.currentTimeMillis();
+        long deltaTime = time - previousTime;
         previousTime = time;
-        previousIntegral += error * deltatime;
+        previousIntegral += error * deltaTime;
         double output = 
             projected * error +
             previousIntegral +
-            (error-previousError) / deltatime;
+            (error-previousError) / deltaTime;
         previousError = error;
-        return output
+        return output;
     }
 }
