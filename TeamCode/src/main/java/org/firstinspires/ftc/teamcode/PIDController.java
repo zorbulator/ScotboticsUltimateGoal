497d2a3e3306d5a1cacb01ad1aa8a82d955960aa
Clayton-Toste <claytontoste@gmail.com>
2020-07-30 20:50:09 -0700

    Added comments, improved formatting and fixed formula for PIDController.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -1,24 +1,26 @@
 import java.lang.System;   
 
+
 public class PIDController {
+
     private final double projected, integral, derivative;
-    private long timePrior = System.currentTimeMillis();
-    private double errorPrior = 0;
+    private long       timePrior = System.currentTimeMillis();
+    private double    errorPrior = 0;
     private double integralPrior = 0;
 
     public PIDController(double projected, double integral, double derivative)
     {
-        this.projected = projected;
-        this.integral = integral;
+        this.projected  = projected;
+        this.integral   = integral;
         this.derivative = derivative;
     }
 
+    // Caculate error correction and update "Prior" values.
     public double update(double error)
     {
         long deltaTime = -timePrior + (timePrior=System.currentTimeMillis());
-        return
-            projected * error +
-            (integralPrior += error * deltaTime) +
-            (-errorPrior + (errorPrior=error)) / deltaTime;
+        return projected  * error +
+               integral   * (integralPrior += error * deltaTime) +
+               derivative * (-errorPrior + (errorPrior=error)) / deltaTime;
     }
 }
